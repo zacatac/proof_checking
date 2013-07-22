ae2d22cb542ba8c6a59c5fe01f87772265d6487e
SherryXu <sherry.xu@berkeley.edu>
2013-07-22 10:48:55 -0700

    Update Proof.java

--- src/Proof.java
+++ src/Proof.java
@@ -9,8 +9,57 @@ public class Proof {
         return null;
     }
 
-    public void extendProof (String x) throws IllegalLineException, IllegalInferenceException {
-    }
+		// return line.next()
+	}
+
+	public void extendProof (String x) throws IllegalLineException, IllegalInferenceException {
+		String[] splitted = x.split (" ");
+		Stack characters = new Stack( );
+			for(int k=0; k < splitted.length; k++) {
+				if(splitted[k].charAt(0) == '(') { 
+					Expression NewExpress = new Expression(splitted[k]);
+					if (splitted[k].charAt(0) == '(') {
+						charcters.push(parts[k].charAt(0));
+						//opening parenthesis on stack
+						//**Check for nested parenthesis (not done, maybe it would be easier to use a tree for this 
+                        //or some kind of recursion)
+					}
+					if (splitted[k].charAt(0) == ')') {
+						characters.pop();
+						
+						if (characters.empty() == false) {
+							//stack should be empty at end
+							//(same number of parens pushed on and popped off)
+							throw new IllegalLineException();
+							
+							
+						else if(splitted[0].equals("assume")) {
+							//fill
+							}
+						else if(splitted[0].equals("co")) {
+							//fill
+							}
+						else if(splitted[0].equals("show")) {
+							//fill
+							}
+						else if(splitted[0].equals("mt")) {
+							//fill
+							}
+						else if(splitted[0].equals("mp")) {
+							//fill
+							}
+						else if(splitted[0].equals("repeat")) {
+							//fill
+							}
+						
+					}
+				}
+				
+			}
+		
+			
+		
+	}
 
     public String toString ( ) {
         return "";
