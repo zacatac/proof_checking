106ff2990e37a3c5a5efdd00f40a153c0d2c4f04
CherylHe <cherylheberkeley@gmail.com>
2013-07-25 11:04:26 -0700

    Update ExpressionTest.java
    1st commit test

--- tests/ExpressionTest.java
+++ tests/ExpressionTest.java
@@ -5,5 +5,17 @@
  * Time: 10:58 AM
  * To change this template use File | Settings | File Templates.
  */
-public class ExpressionTest {
+import junit.framework.TestCase;
+
+
+public class ExpressionTest extends TestCase {
+  
+	public void testTree(){
+		Expression myExpr = new Expression("((p=>q)=>(c&d))");
+		
+		System.out.print(myExpr.fullExpr);
+		BinaryTree b = new BinaryTree();
+		BinaryTree expressionTree = b.exprTree(myExpr.fullExpr);
+		expressionTree.print();
+	}
 }
