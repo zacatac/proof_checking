93e0127852cd4f527ec250934180d823e7220978
thetedd <theted@berkeley.edu>
2013-07-24 23:12:32 -0700

    Update BinaryTree.java
    now with pattern matching (or so I hope)

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.TreeMap;
+
 public class BinaryTree {
 	public int height() {
 		return height(myRoot);
@@ -12,7 +14,7 @@ public class BinaryTree {
 	public BinaryTree ( ) {
 		myRoot = null;
 	}
-	
+
 	public BinaryTree (TreeNode t) {
 		myRoot = t;
 	}
@@ -28,23 +30,62 @@ public class BinaryTree {
     public TreeNode getMyRight() {
         return myRoot.getMyRight();
     }
+    public boolean canbematched(BinaryTree theoremtree) {
+        TreeMap<Object, TreeNode> test = matchhelper(theoremtree.myRoot, new TreeMap<Object, TreeNode>());
+        if (test.containsKey("false")) {
+            return false;
+        }
+        return true;
+    }
+    private TreeMap<Object, TreeNode> matchhelper(TreeNode thm, TreeMap t) {
+        if (thm==null) {
+            return t;
+        }
+        if (thm.myItem=="=>"||thm.myItem=="&"||thm.myItem=="|"||thm.myItem=="~") {
+            if (thm.myItem!=myRoot.myItem) {
+                t.put("false", myRoot);
+                return t;
+            }
+        } else {
+            if (!t.containsKey(thm.myItem)) {
+                  t.put(thm.myItem, myRoot);
+            } else {
+                if (!equals(myRoot, (TreeNode)t.get(thm.myItem))) {
+                    t.put("false", myRoot);
+                    return t;
+                }
+            }
+        }
+        t = new BinaryTree(getMyLeft()).matchhelper(thm.myLeft, t);
+        t = new BinaryTree(getMyRight()).matchhelper(thm.myRight, t);
+        return t;
+    }
 
 	public static void main (String [ ] args) {
 		BinaryTree t = new BinaryTree (new TreeNode("a", new TreeNode("b", new TreeNode("d"), null), new TreeNode("c")) );
-		t.print();
-		System.out.println(t.height());
+		//t.print();
+		//System.out.println(t.height());
 		BinaryTree t2 = new BinaryTree (new TreeNode("a", new TreeNode("b", new TreeNode("d"), null), new TreeNode("c")) );
-		System.out.println(t.equals(t2));//yes this is True
+		//System.out.println(t.equals(t2));//yes this is True
 		BinaryTree t3 = new BinaryTree (new TreeNode("a", new TreeNode("b", new TreeNode("e"), null), new TreeNode("c")) );
-		System.out.println(t.equals(t3));//yes this is False
+		//System.out.println(t.equals(t3));//yes this is False
+		BinaryTree thm = new BinaryTree (new TreeNode("=>", new TreeNode("p"), new TreeNode("&", new TreeNode("p"), new TreeNode("q"))));
+		thm.print();
+		BinaryTree test = new BinaryTree (new TreeNode("=>", new TreeNode("|", new TreeNode("a"), new TreeNode("b")), new TreeNode("&", new TreeNode("|", new TreeNode("a"), new TreeNode("b")), new TreeNode("c"))));
+				
+				//new TreeNode(, new TreeNode("&", new TreeNode(new TreeNode("|", new TreeNode("a"), new TreeNode("b")), new TreeNode("c")))));
+		test.print();
+		BinaryTree test2 = new BinaryTree (new TreeNode("=>", new TreeNode("|", new TreeNode("c"), new TreeNode("b")), new TreeNode("&", new TreeNode("|", new TreeNode("a"), new TreeNode("b")), new TreeNode("c"))));
+		System.out.println(test.canbematched(thm));//is True!
+		System.out.println(test2.canbematched(thm));//is False!
 	}
 
 	public static class TreeNode {
-		
+
 		private Object myItem;
 		private TreeNode myLeft;
 		private TreeNode myRight;
-		
+
 		public Object getMyItem() {
             return myItem;
         }
@@ -75,9 +116,9 @@ public class BinaryTree {
 	        printHelper (myRoot, 0);
 	    }
 	}
-		
+
 	private static final String indent1 = "    ";
-		
+
 	private static void printHelper (TreeNode root, int indent) {
 		if (root!=null) {
 		    printHelper(root.myRight, indent + 4); //mine
@@ -85,7 +126,7 @@ public class BinaryTree {
 		    printHelper(root.myLeft, indent + 4); //mine
 		}
 	}
-			
+
 	private static void println (Object obj, int indent) {
 	    for (int k=0; k<indent; k++) {
 	        System.out.print (indent1);
@@ -126,4 +167,3 @@ public class BinaryTree {
 	}
 
 }
-
