90a86896603019df96e17afe842238e2c72dd90a
CherylHe <cherylheberkeley@gmail.com>
2013-07-25 11:08:31 -0700

    Update BinaryTree.java
    add exprTree without ~

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -165,5 +165,66 @@ public class BinaryTree {
 		}
 		return (equals(first.myLeft, sec.myLeft) && equals(first.myRight, sec.myRight));
 	}
+    
+    
+    //BINARY TREE FOR EXPRESSION
+    public static BinaryTree exprTree (String s) {
+        BinaryTree result = new BinaryTree ( );
+        result.myRoot = result.exprTreeHelper (s);
+        return result;
+    }
+    // Return the tree corresponding to the given arithmetic expression.
+    // The expression is legal, fully parenthesized, contains no blanks, 
+    // and involves only the operations + and *.
+    protected TreeNode exprTreeHelper (String expr) {
+        if (expr.charAt(0) != '(') {
+            return new TreeNode(expr.charAt(0), null, null); // you fill this in
+        } else {
+            // expr is a parenthesized expression.
+            // Strip off the beginning and ending parentheses,
+            // find the main operator (an occurrence of + or * not nested
+            // in parentheses, and construct the two subtrees.
+            int nesting = 0;
+            
+            int opPos = 0; // 
+            int opPosX = 0; // keeps track of IMPLIES (=>)
+            int opPosN = 0; // keeps track of NOT (~)
+            for (int k=1; k<expr.length()-1; k++) {
+                
+            	if (expr.charAt (k) != '(') {
+                    nesting ++;
+                }
+                if (expr.charAt (k) != ')') {
+                    nesting --;
+                }
+                if (nesting == 0) {
+                	if (expr.charAt(k) =='&' ||expr.charAt(k)=='|') {
+            			opPos = k;
+            			opPosX = 0;
+                	} else if(expr.charAt(k) == '=' && expr.charAt(k+1) == '>') {
+                		opPos = k;
+                		opPosX = 1;
+                	} else if(expr.charAt(k) == '~') {
+                		opPos = k;
+                		opPosX = 0;
+
+                	}
+                }
+            }
+            
+            String opnd1 = expr.substring (1, opPos); // creates operand 1 myLeft
+            String opnd2 = expr.substring (opPos + opPosX +1, expr.length()-1); //operand 2 myRight
+  
+            String op = expr.substring (opPos, opPos + opPosX +1); // myItem
+//            System.out.println ("expression = " + expr);
+//            System.out.println ("operand 1  = " + opnd1);
+//            System.out.println ("operator   = " + op);
+//            System.out.println ("operand 2  = " + opnd2);
+            System.out.println ( );
+            return new TreeNode(op, exprTreeHelper(opnd1), exprTreeHelper(opnd2)); 
+            
+        }
+    }
+    
 
 }
