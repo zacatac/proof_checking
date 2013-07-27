b5a0e87bc612bc5a515c7a8766fc0dd6628fa1b0
thetedd <theted@berkeley.edu>
2013-07-27 08:38:12 -0700

    Update TheoremSet.java

--- src/TheoremSet.java
+++ src/TheoremSet.java
@@ -11,6 +11,9 @@ public class TheoremSet {
     }
 
     public Expression put (String s, Expression e) {
+         //so Expression class should have a method that calls Binary Tree's exprTreeHelper method
+         // and we'll call that on e, and pass the resulting binary tree along with String s into a bundle constructor
+         //and add this bundle into myTheorems.
         return null;
     }
 }
