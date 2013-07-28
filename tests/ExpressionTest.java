22bc03fc37788c1060611c63fe2a8dba670a1552
thetedd <theted@berkeley.edu>
2013-07-28 12:52:12 -0700

    Update ExpressionTest.java

--- tests/ExpressionTest.java
+++ tests/ExpressionTest.java
@@ -7,13 +7,21 @@ public class ExpressionTest extends TestCase {
 		Expression myExpr = null;
 		Expression myExpr2 = null;
 		Expression myExpr3 = null;
+		Expression myExpr4 = null;
+		Expression myExpr5 = null;
+		Expression myExpr6 = null;
+		Expression parenest = null;
 		
 		try{ 
 			myExpr = new Expression("((p=>q)=>(c&d))");
 			myExpr2 = new Expression("((~p=>q)=>c)");
 			myExpr3 = new Expression("(~a=>~~~b)");
+			myExpr4 = new Expression("(~(a|b)=>~~b)");
+			myExpr5 = new Expression("(a=>~(~b))");
+			myExpr6 = new Expression("(~(b=>(a|b)))");
+			parenest = new Expression("(~a=>b)");
 		} catch (IllegalLineException e){
-			
+			System.out.println(e);
 		}
 		System.out.println(myExpr.fullExpr);
 		
@@ -33,8 +41,31 @@ public class ExpressionTest extends TestCase {
 		expressionTree = myExpr3.exprTree(myExpr3.fullExpr);
 		System.out.println();
 		expressionTree.print();
+		System.out.println("Testing tree 4");
 		
+		System.out.println(myExpr4.fullExpr);
+		//BinaryTree d = new BinaryTree();
+		expressionTree = myExpr4.exprTree(myExpr4.fullExpr);
+		System.out.println();
+		expressionTree.print();
 		
+		System.out.println(myExpr5.fullExpr);
+		//BinaryTree d = new BinaryTree();
+		expressionTree = myExpr5.exprTree(myExpr5.fullExpr);
+		System.out.println();
+		expressionTree.print();
+		
+		System.out.println(myExpr6.fullExpr);
+		//BinaryTree d = new BinaryTree();
+		expressionTree = myExpr6.exprTree(myExpr6.fullExpr);
+		System.out.println();
+		expressionTree.print();
+		
+		System.out.println(parenest.fullExpr);
+		//BinaryTree d = new BinaryTree();
+		expressionTree = parenest.exprTree(parenest.fullExpr);
+		System.out.println();
+		expressionTree.print();
 	}
 	
 }
