b608052a2bb83fad41e2db7208e91bd2445a6507
thetedd <theted@berkeley.edu>
2013-07-28 07:39:12 -0700

    Update ExpressionTest.java

--- tests/ExpressionTest.java
+++ tests/ExpressionTest.java
@@ -12,22 +12,26 @@ public class ExpressionTest extends TestCase {
 			myExpr = new Expression("((p=>q)=>(c&d))");
 			myExpr2 = new Expression("((~p=>q)=>c)");
 			myExpr3 = new Expression("(~a=>~~~b)");
-		} catch (IllegalLineException e){
+		} catch (IllegalArgumentException e){
 			
 		}
-		System.out.println(myExpr.fullExpr);		
-		BinaryTree b = new BinaryTree();
-		BinaryTree expressionTree = b.exprTree(myExpr.fullExpr);
+		System.out.println(myExpr.fullExpr);
+		
+		//BinaryTree b = new BinaryTree();
+		BinaryTree expressionTree = myExpr.exprTree(myExpr.fullExpr);
+		System.out.println();
 		expressionTree.print();
 
 		System.out.println(myExpr2.fullExpr);
-		BinaryTree c = new BinaryTree();
-		expressionTree = c.exprTree(myExpr2.fullExpr);
+		//BinaryTree c = new BinaryTree();
+		expressionTree = myExpr2.exprTree(myExpr2.fullExpr);
+		System.out.println();
 		expressionTree.print();
 		
 		System.out.println(myExpr3.fullExpr);
-		BinaryTree d = new BinaryTree();
-		expressionTree = d.exprTree(myExpr3.fullExpr);
+		//BinaryTree d = new BinaryTree();
+		expressionTree = myExpr3.exprTree(myExpr3.fullExpr);
+		System.out.println();
 		expressionTree.print();
 		
 		
