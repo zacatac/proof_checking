1601d449708f3aba9dc1ffdbed7b2c10cb55d660
CherylHe <cherylheberkeley@gmail.com>
2013-07-27 22:33:01 -0700

    expression tree/test works for everything except things without parens

--- tests/ExpressionTest.java
+++ tests/ExpressionTest.java
@@ -1,14 +1,17 @@
 import junit.framework.TestCase;
 
-
+//EVERYTHING WITH PARENS WORKS BUT NOTHING WITHOUT THEM WORKS THIS IS SO SAD IM NARRATING MY LIFE D:
 public class ExpressionTest extends TestCase {
 	
 	public void testTree(){
 		Expression myExpr = null;
 		Expression myExpr2 = null;
+		Expression myExpr3 = null;
+		
 		try{ 
 			myExpr = new Expression("((p=>q)=>(c&d))");
 			myExpr2 = new Expression("((~p=>q)=>c)");
+			myExpr3 = new Expression("(~a=>~~~b)");
 		} catch (IllegalLineException e){
 			
 		}
@@ -22,7 +25,13 @@ public class ExpressionTest extends TestCase {
 		expressionTree = c.exprTree(myExpr2.fullExpr);
 		expressionTree.print();
 		
+		System.out.println(myExpr3.fullExpr);
+		BinaryTree d = new BinaryTree();
+		expressionTree = d.exprTree(myExpr3.fullExpr);
+		expressionTree.print();
+		
 		
 	}
 	
 }
+
