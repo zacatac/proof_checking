d1b6e1418eca47dad3476572e2060417777bdb38
SherryXu <sherry.xu@berkeley.edu>
2013-07-28 17:32:53 -0700

    Update ExpressionTest.java

--- tests/ExpressionTest.java
+++ tests/ExpressionTest.java
@@ -44,25 +44,25 @@ public class ExpressionTest extends TestCase {
 		
 		String myString5;
 		try {
-		myString5 = new String("$ == #");
+		myString5 = new String("$");
 		Expression sdfgsdfg = new Expression(myString5);
 		fail("Should have caught illegal character");
 		} catch (IllegalLineException e) {
 			
 		}
 		
-		String myString6;
-		try {
-		myString6 = new String("ap == bd");
-		Expression sdfgsdfg = new Expression(myString6);
-		fail("Should not have two characters that are not special characters next to eachother");
-		} catch (IllegalLineException e) {
+		//String myString6;
+		//try {
+		//myString6 = new String("a => d");
+		//Expression sdfgsdfg = new Expression(myString6);
+		//fail("no parens");
+		//} catch (IllegalLineException e) {
 			
-		}
+		//}
 		
 		String myString7;
 		try {
-		myString7 = new String("ap == bd");
+		myString7 = new String("ap");
 		Expression sdfgsdfg = new Expression(myString7);
 		fail("Should not have two characters that are not special characters next to eachother");
 		} catch (IllegalLineException e) {
@@ -78,7 +78,48 @@ public class ExpressionTest extends TestCase {
 			
 		}
 		
+		String myString9;
+		try {
+		myString9 = "((a)";
+		Expression sdfgsdfg = new Expression(myString9);
+		fail("Should have caught unequal parens Left");
+		} catch (IllegalLineException e) {
+			
+		}
+		
+		String myString10;
+		try {
+		myString10 = "(a";
+		Expression sdfgsdfg = new Expression(myString10);
+		fail("Should have caught unequal parens Right, things need to be closed with parens");
+		} catch (IllegalLineException e) {
+			
+		}
+
+		
+		String myString11;
+		try {
+		myString11 = "(p=>(~p=>q))";
+		Expression sdfgsdfg = new Expression(myString11);
+		} catch (IllegalLineException e) {
+			fail("What's supposed to work didn't work");
+		}
+			
+		
+
+		String myString12;
+		try {
+		myString12 = "(p=>(~p=>q))";
+		Expression sdfgsdfg = new Expression(myString12);
+		} catch (IllegalLineException e) {
+			fail("What's supposed to work didn't work");
+		}
+			
+		
+
+		
 	}
+}
 	
 	
 	
